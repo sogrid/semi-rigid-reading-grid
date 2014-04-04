4652a9a8cc6e27575d7fd3c1009bf64022091748
tappily <tappily.b@gmail.com>
2014-04-04 04:54:45 -0700

    Initial revision.

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        clean: {
+            dist: ['dist']
+        },
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['less/loop.less'],
+                    dest: 'dist/',
+                    filter: 'isFile'
+                }]
+            }
+        },
+        csslint: {
+            options: {
+                csslintrc: '.csslintrc'
+            },
+            strict: {
+                src: ['dist/css/*.css']
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            gruntfile: ['Gruntfile.js']
+        },
+        less: {
+            options: {
+                strictMath: true,
+                strictUnits: true
+            },
+            specimen: {
+                files: [
+                    {'dist/css/specimen.css': 'src/less/specimen/*.less'}
+                ]
+            }
+        },
+        release: {
+            options: {
+                file: 'bower.json'
+            }
+        },
+        watch: {
+            less: {
+                files: 'src/less/**/*.less',
+                tasks: ['less', 'csslint']
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('test', ['jshint', 'csslint']);
+    grunt.registerTask('build', ['clean', 'less', 'test', 'copy']);
+};
